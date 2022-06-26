4701cdad44ffced8023daae27ecf68b986864ee4
User <akmaljoraqulov1@gmail.com>
2022-06-26 08:59:26 +0500

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app_611/pages/first_page.dart';
 import 'package:app_611/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
@@ -10,14 +11,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: const FirstPage(),
       routes: {
+        FirstPage.id:(context)=>const FirstPage(),
         HomePage.id:(context)=>const HomePage(),
       },
     );
